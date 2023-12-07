5f4c370d4ccc4f9ca0ad87ada1e2532df49eba6c
Rémi Petitpierre <38254995+RPetitpierre@users.noreply.github.com>
2023-12-07 12:38:49 +0100

    Add files via upload

--- assets/js/icono.js
+++ assets/js/icono.js
@@ -5,24 +5,7 @@ var transparent = false;
 var markersCluster = new L.MarkerClusterGroup();
 var firstTransparencyActivation = true, firstVectorizedActivation = true;
 
-var slider = document.getElementById("selectYear");
-var output = document.getElementById("slider_year");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-var redIcon = new L.Icon({
-	iconUrl: 'assets/img/marker-icon-2x-red.png',
-	shadowUrl: 'assets/img/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
-});
-
-var CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+var CartoDB_LightMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
 	maxZoom: 19
@@ -45,32 +28,109 @@ function initRequest(requestURL){
 	return request;
 }
 
+// Link from the Museris database
+function getQueryParam(param) {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
 
-function updateMap(){
+init();
 
-	map.removeLayer(markersCluster);
-	markersCluster = new L.MarkerClusterGroup();
+var slider = document.getElementById('yearRange');
 
-	selectedYear = document.getElementById("selectYear").value*10;
+noUiSlider.create(slider, {
+    start: [1830, 1870], // starting values
+    connect: true, // shades the area between handles
+    range: {
+        'min': 1740,
+        'max': 2010
+    },
+    margin: 1
+});
 
-	var request = initRequest('https://bnf-jadis.github.io/assets/data/geocoded_MHL.json');
-	request.onload = function() {
+var selectedStartYear = 1830;
+var selectedEndYear = 1870;
 
-		var jsonObject = request.response;
-		var subset = $.grep(jsonObject, function( n, i ) {
-		  return (n.start_year >= selectedYear) && (n.end_year < (selectedYear+10));
-		});
+slider.noUiSlider.on('set', function (values, handle) {
+
+    selectedStartYear = parseInt(values[0]);
+    selectedEndYear = parseInt(values[1]);
+
+    updateMap(selectedStartYear, selectedEndYear);
+});
+
+slider.noUiSlider.on('update', function (values, handle) {
+    selectedStartYear = parseInt(values[0]);
+    selectedEndYear = parseInt(values[1]);
+    document.getElementById("slider_year").textContent = String(selectedStartYear) + ' – ' + String(selectedEndYear);
+});
+
+function updateMap(selectedStartYear=1740, selectedEndYear=2010, imageIdParam=null){
+
+    var request = initRequest('https://bnf-jadis.github.io/assets/data/geocoded_MHL.json');
+    request.onload = function() {
+
+        var jsonObject = request.response;
+
+        if (imageIdParam) {
+
+        	jsonObject.forEach(function(item) {
+        		let id = item.image_url.split(';id=')[1];
+		        if (id === imageIdParam) {
+		            map.setView([item.lat, item.lon], 17); // Center and zoom
+					selectedStartYear = Math.max(1740, item.start_year - 5);
+					selectedEndYear = Math.min(2010, item.end_year + 5);
+		            slider.noUiSlider.set([selectedStartYear, selectedEndYear]); // Adjust the slider
+		        }
+		    });
+		}
+
+		map.removeLayer(markersCluster);
+    	markersCluster = new L.MarkerClusterGroup();
+
+        var subset = $.grep(jsonObject, function( n, i ) {
+          return (n.start_year >= selectedStartYear) && (n.end_year <= selectedEndYear);
+        });
+
+        console.log(subset.length);
 		
 		for (i=0; i < subset.length; i++){
-			id = subset[i].image_url.split(';id=')[1]
+			var redIcon = new L.Icon({
+			  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+			  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+			  iconSize: [25, 41],
+			  iconAnchor: [12, 41],
+			  popupAnchor: [1, -34],
+			  shadowSize: [41, 41]
+			});
+			let id = subset[i].image_url.split('amp;id=')[1];
+			let title = subset[i].titre;
+			if (title.length > 150) {
+				title = title.slice(0, 150) + '...';
+			}
 
-			var marker = new L.marker([subset[i].lat, subset[i].lon]).bindPopup(
-				'<img src="' + subset[i].image_url + '" height="' + String(~~(subset[i].shape_x/1.5)) + 'px" width="' + String(
-				~~(subset[i].shape_y/1.5)) + 'px"/><br><center><b>' + subset[i].titre + '<br><a href="https://museris.lausanne.ch/SGCM/Consultation.aspx?id=' + id + '">En savoir plus</b></center>');
-			marker.setIcon(redIcon);
+			let height = String(~~(subset[i].shape_x/1.5));
+			let width = String(~~(subset[i].shape_y/1.5));
+			let htmlContent = '<img src="' + subset[i].image_url + '" height="' + String(~~(subset[i].shape_x/1.5)) + 'px" width="' + String(
+				~~(subset[i].shape_y/1.5)) + 'px"/><br><center><b>' + title + '<br><a href="https://museris.lausanne.ch/SGCM/Consultation.aspx?id=' + id + '">En savoir plus</b></center>';
+			var marker = new L.marker([subset[i].lat, subset[i].lon], {icon: redIcon}).bindPopup(
+				htmlContent, {maxWidth: width, minWidth: width});
 			markersCluster.addLayer(marker);
+
+			/*if (id === imageIdParam) {
+				marker.openPopup();
+			}*/
 		}
 		map.addLayer(markersCluster);
+
+		/*if (imageIdParam) {
+        	markersCluster.eachLayer(function(layer) {
+                if (layer.getPopup().getContent().includes('amp;id=' + imageIdParam)) {
+                	console.log('found', layer.getPopup().getContent());
+                    layer.openPopup();
+            	}
+			});
+		}*/
 	}
 }
 
@@ -81,7 +141,7 @@ function init(){
 	    center: [46.519653, 6.632273],
 	    zoom: 14,
 	    minZoom: 12,
-	    layers: [CartoDB_DarkMatterNoLabels],
+	    layers: [CartoDB_LightMatter],
 	    fullscreenControl: true,
 	    fullscreenControlOptions: {
 	        title:"Plein écran"
@@ -90,7 +150,7 @@ function init(){
 	});
 
 	// Initialize map and year select buttons
-	updateMap()
+	//updateMap()
 
 	var attributionControl = L.control({
 	  position: "bottomright"
@@ -104,10 +164,17 @@ function init(){
 
 	// Add scale bar
 	L.control.scale().addTo(map);
+
+	var imageIdParam = getQueryParam('id');
+	if (imageIdParam) {
+	    updateMap(null, null, imageIdParam);
+	} else {
+	    updateMap(); // Call without parameters for default behavior
+	}
+
 }
 
-init()
-
+/*
 // Location
 map.locate({setView: true, maxZoom: 15});
 
@@ -125,5 +192,4 @@ function onLocationFound(e) {
 map.on('locationfound', onLocationFound);
 function onLocationError(e) { }
 
-map.on('locationerror', onLocationError);
-
+map.on('locationerror', onLocationError);*/
